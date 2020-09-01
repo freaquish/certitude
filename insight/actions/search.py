a009aefa8819e43c9dbc66d2aa1361e9658274b1
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-09-01 16:19:01 +0530

    Notification Model

--- insight/actions/search.py
+++ insight/actions/search.py
@@ -4,41 +4,27 @@ from django.db.models import DecimalField, F, ExpressionWrapper, Value
 from django.db.models.functions import Concat
 from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
+"""
+ search atags : Post containing atags, Account, community, competiton related with query
+ search hastags
+ search first_name, last_name
+ search hobby
+ search competition
+ search community
+"""
 
-class Search:
 
-    def __init__(self, **kwargs):
-        if kwargs:
-            if 'account_id' in kwargs:
-                self.account_id = kwargs['account_id']
-            if 'account' in kwargs:
-                self.account = kwargs['account']
-            else:
-                self.account = None
-            if 'query' in kwargs:
-                self.query = kwargs['query']
+class SearchEngine:
+    def __init__(self, user, query: str):
+        self.user: Account = user
+        self.query: str = query
 
-    def search_tags(self):
-        return Tags.objects.filter(Q(tag__search=self.query)).annotate(rank=SearchRank(SearchVector('tag'),
-                                                                                       SearchQuery(
-                                                                                           self.query))).order_by(
-            '-rank')
+    @staticmethod
+    def sanitize_query(query: str):
+        if '@' in query:
+            return query.replace('@', '')
+        elif '#' in query:
+            return query.replace('#', '')
 
-    def search_accounts(self):
-        vector = SearchVector('first_name') + SearchVector('last_name')
-        query = SearchQuery(self.query)
-        if not self.account:
-            return Account.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name')).filter(
-                full_name__search=self.query
-            ).annotate(rank=SearchRank(vector, query)).order_by('-rank', '-influencer')
-        else:
-            return Account.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name')).filter(
-                full_name__search=self.query
-            ).annotate(rank=SearchRank(vector, query),
-                       hobby_distance=ExpressionWrapper(F('primary_weight') - DecimalField(self.account.primary_weight))
-                       ).order_by('-rank', '-influencer', '-hobby_distance')
-
-    def search_hobby(self):
-        vector = SearchVector('name')
-        query = SearchQuery(self.query)
-        return Hobby.objects.filter(name__search=self.query).annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    def search_user(self):
+        return Account.objects.filter(Q(username__search=query))
--- insight/models.py
+++ insight/models.py
@@ -14,7 +14,7 @@ from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 class AccountManager(BaseUserManager):
 
-    def create_user(self, account_id,password, **extra_fields):
+    def create_user(self, account_id, password, **extra_fields):
         account = self.model(account_id, **extra_fields)
         account.set_password(password)
         account.save()
@@ -36,25 +36,31 @@ class AccountManager(BaseUserManager):
 
 class Account(AbstractBaseUser, PermissionsMixin):
     # emailId or phone number without country code
-    account_id = models.CharField(max_length=50, default='account_id', primary_key=True)
+    account_id = models.CharField(
+        max_length=50, default='account_id', primary_key=True)
     id_type = models.CharField(max_length=6, default='PHONE')  # PHONE or EMAIL
     joined_at = models.DateField(default=get_ist_date())
-    username = models.CharField(max_length=30, default='', unique=True, db_index=True)
+    username = models.CharField(
+        max_length=30, default='', unique=True, db_index=True)
     first_name = models.CharField(max_length=30, default='')
     last_name = models.CharField(max_length=30, default='')
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    details = JSONField(default=dict)  # All personal details such as phone number, email, address, school. organisation
-    comfort_zones_text = ArrayField(models.CharField(max_length=70), default=list)  # names of landmarks as keywords
-    activity_coords = JSONField(default=dict)  # {'x,y':{x:coords.x,y:coords.y,hit:n}}
+    # All personal details such as phone number, email, address, school. organisation
+    details = JSONField(default=dict)
+    comfort_zones_text = ArrayField(models.CharField(
+        max_length=70), default=list)  # names of landmarks as keywords
+    # {'x,y':{x:coords.x,y:coords.y,hit:n}}
+    activity_coords = JSONField(default=dict)
     avatar = models.TextField()
     places = ArrayField(models.TextField(), default=list)
     influencer = models.BooleanField(default=False)
     influencing_hobby = models.CharField(max_length=20, default='')
-    hobby_map = JSONField(default=dict) #{"code_name": E(action)}
+    hobby_map = JSONField(default=dict)  # {"code_name": E(action)}
     primary_hobby = models.CharField(max_length=20, default='')
-    primary_weight = models.DecimalField(max_digits=4, decimal_places=2, default=0.00)
+    primary_weight = models.DecimalField(
+        max_digits=4, decimal_places=2, default=0.00)
     follower_count = models.IntegerField(default=0)
     following_count = models.IntegerField(default=0)
     saves = ArrayField(models.CharField(max_length=30), default=list)
@@ -63,13 +69,13 @@ class Account(AbstractBaseUser, PermissionsMixin):
     description = models.TextField()
     following = ArrayField(models.CharField(max_length=30), default=list)
     friend = ArrayField(models.CharField(max_length=30), default=list)
-    current_coord = gis_models.PointField(default=Point(0, 0, srid=4326), srid=4326)
+    current_coord = gis_models.PointField(
+        default=Point(0, 0, srid=4326), srid=4326)
     new_notification = models.BooleanField(default=False)
     USERNAME_FIELD = 'account_id'
     REQUIRED_FIELDS = ('id_type', 'joined_at')
 
     objects = AccountManager()
-
 
     def insert_coords(self, inserted_coords: Point, saveData=True):
         coord_x, coord_y = inserted_coords
@@ -81,11 +87,12 @@ class Account(AbstractBaseUser, PermissionsMixin):
                 insertion_required = False
         if insertion_required:
             self.activity_coords[f'{inserted_coords[0]},{inserted_coords[1]}'] = {'x': inserted_coords[0],
-                                                                                        'y': inserted_coords[1],
-                                                                                        'hit': 1}
+                                                                                  'y': inserted_coords[1],
+                                                                                  'hit': 1}
         self.current_coord = inserted_coords
         if saveData:
             self.save()
+
 
 class Hobby(models.Model):
     code_name = models.CharField(max_length=30, default='', primary_key=True)
@@ -99,7 +106,8 @@ class Post(models.Model):
     post_id = models.CharField(max_length=22, primary_key=True, default='')
     username = models.CharField(max_length=30, default='')
     # account_id = models.CharField(max_length=50, default='')
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, default='account_id')
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, default='account_id')
     editor = models.CharField(max_length=14)
     # hobby = models.CharField(max_length=30, default='')
     hobby = models.ForeignKey(Hobby, on_delete=models.CASCADE, default='')
@@ -107,17 +115,16 @@ class Post(models.Model):
     caption = models.TextField()
     hastags = ArrayField(models.CharField(max_length=20), default=list)
     atags = ArrayField(models.CharField(max_length=20), default=list)
-    coords = gis_models.PointField(Point(0, 0, srid=4326), srid=4326, blank=True,null=True)
+    coords = gis_models.PointField(
+        Point(0, 0, srid=4326), srid=4326, blank=True, null=True)
     action_count = JSONField(default=dict)
     created_at = models.DateTimeField(default=get_ist())
     rank = models.IntegerField(default=0)
     score = models.DecimalField(max_digits=7, decimal_places=3, default=0.0)
 
 
-
-
 class UserActionRef(models.Model):
-    account_id = models.CharField(max_length = 50, default= '')
+    account_id = models.CharField(max_length=50, default='')
     date_created = models.DateField(default=get_ist_date())
     time_created = models.TimeField(default=get_ist_time())
     hobby = models.CharField(max_length=20, default='')
@@ -139,20 +146,20 @@ class ActionStore(models.Model):
     saved = models.BooleanField(default=False)
     commented = models.BooleanField(default=True)
 
-    def update(self,**kwargs):
+    def update(self, **kwargs):
         for key in kwargs.keys():
             self.__dict__[key] = kwargs[key]
-        self.save()                     
+        self.save()
 
 
 class Notification(models.Model):
-    accounts = ArrayField(models.CharField(max_length=50), default=list)
-    created = models.DateTimeField(default=get_ist())
-    notification_type = models.CharField(max_length=20, default=GENERAL_ACTIVITY)
-    header = models.TextField()
-    body = models.TextField()
-    image = models.TextField()
+    type = models.CharField(max_length=5, default='ALERT')
     meta = JSONField(default=dict)
+    header = models.TextField()
+    created_at = models.DateTimeField(default=get_ist())
+    body = models.TextField()
+    to = models.ForeignKey(Account, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
 
 
 class PostComment(models.Model):
@@ -166,7 +173,8 @@ class Leaderboard(models.Model):
     end_date = models.DateField(default=get_ist_date())
     hobby = models.CharField(max_length=20, default='')
     hobby_name = models.CharField(max_length=30, default='')
-    rank_list = JSONField(default=dict)  # rank: {account_id,score,name,influencer}
+    # rank: {account_id,score,name,influencer}
+    rank_list = JSONField(default=dict)
 
 
 class Tags(models.Model):
@@ -179,12 +187,14 @@ class Places(models.Model):
     place_name = models.TextField()
     city = models.TextField()
 
+
 class RankBadge(models.Model):
     competition_name = models.TextField()
     date_field = models.DateField(default=get_ist_date())
     time_field = models.TimeField(default=get_ist_time())
     hobby = models.ForeignKey(Hobby, on_delete=models.CASCADE, default='')
-    account = models.ForeignKey(Account, on_delete=models.CASCADE, default='account_id')
+    account = models.ForeignKey(
+        Account, on_delete=models.CASCADE, default='account_id')
     total = models.IntegerField(max_length=5)
-    rank = models.IntegerField(max_length=5,default=0)
+    rank = models.IntegerField(max_length=5, default=0)
     score = models.DecimalField(max_digits=7, decimal_places=3, default=0.0)
