61ae1901ce268354a7f44629d55ddd0397d84e70
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2020-10-10 18:41:52 +0530

    merge finished

--- .history/insight/competitions/main_20201010183559.py
+++ .history/insight/competitions/main_20201010183559.py
@@ -12,9 +12,11 @@ from datetime import timedelta
 
 class CompetitionManager:
 
+
     def __init__(self, user: Account, community_id: Community):
         self.user: Account = user
         self.community_id = community_id
+
 
     @staticmethod
     def is_tag_unique(tag):
@@ -24,7 +26,8 @@ class CompetitionManager:
 
         return True
 
-    def create_competition(self, member_id, community_id, **data):
+# creating Comppetition
+    def create_competition(self, member_id, community_id,**data):
         competiton_id: str = token_urlsafe(22) + self.community_id[12:]
         if 'name' in data and 'tag' in data and 'hobbies' in data and 'competition_banner' in data:
             fields = data
@@ -34,18 +37,14 @@ class CompetitionManager:
             fields['result_date'] = get_ist() + timedelta(days=7)
             fields['is_global'] = True if is_global == 1 else False
             fields['is_unique_post'] = True if is_unique_post == 1 else False
-            competiton_exists: QuerySet = Competition.objects.filter(
-                Q(tag=data['tag']) | Q(competiton_id=competiton_id))
-            communities: QuerySet = Community.objects.filter(
-                community_id=community_id)
-            community_members: QuerySet = CommunityMember.objects.filter(
-                Q(Q(account=self.user) | Q(account__account_id=member_id)) & Q(community_id=community_id))
+            competiton_exists : QuerySet = Competition.objects.filter(Q(tag=data['tag']) | Q(competiton_id=competiton_id))
+            communities: QuerySet = Community.objects.filter(community_id=community_id)
+            community_members: QuerySet = CommunityMember.objects.filter(Q(Q(account=self.user) | Q(account__account_id=member_id)) & Q(community_id=community_id))
             """Checking the creator of competition ? Head or Not """
             if communities and len(community_members) == 2 and len(competiton_exists) != 0 and self.is_tag_unique(tag):
-                heads: QuerySet = community_members.filter(is_team_head=True)
+                heads : QuerySet = community_members.filter(is_team_head = True)
                 if heads and heads.first().account.account_id == self.user.account_id:
 
-                    competition = Competition.objects.create_competition(
-                        **fields)
+                    competition = Competition.objects.create_competition(**fields)
 
                     return competition
